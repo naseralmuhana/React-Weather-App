b1153f9fb4e18f74726d31f7485337747e728447
naseralmuhana0 <naseralmuhana0@gmail.com>
2022-06-21 18:03:40 +0300

    Updated!

--- src/components/Search/styled.js
+++ src/components/Search/styled.js
@@ -54,7 +54,7 @@ export const SearchResult = styled.div`
   display: flex;
   flex-direction: column;
   position: absolute;
-  background: ${({ theme }) => theme.searchSuggestion.backgroundColor};
+  background-color: ${({ theme }) => theme.forecastPanelBgColor};
   box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.1);
   width: 98%;
   max-height: 264.6px;
